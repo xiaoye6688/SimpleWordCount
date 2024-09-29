888269ef222ea916afaf715ef0dfecbf21f44168
小夜 <97166212+xiaoye6688@users.noreply.github.com>
2024-09-29 13:21:08 +0800

    提交

--- src/main/scala/myDemo1.scala
+++ src/main/scala/myDemo1.scala
@@ -2,34 +2,22 @@ import org.apache.spark.sql.SparkSession
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    // 创建 SparkSession
     val spark = SparkSession.builder
       .appName("SimpleWordCount")
-      .master("local[*]")  // 本地运行模式，使用所有可用的 CPU 核心
+      .master("local[*]")
       .getOrCreate()
 
-    // 隐式转换，提供 Dataset 中的 Encoder 支持
+    spark.sparkContext.setLogLevel("ERROR")
+
     import spark.implicits._
 
-    // 设置日志级别为WARN
-    spark.sparkContext.setLogLevel("WARN")
+    spark.read.textFile("simple/zhaolongxiao.txt")
+      .flatMap(_.split("\\s+"))
+      .groupByKey(identity)
+      .count()
+      .collect()
+      .foreach { case (word, count) => println(s"$word: $count") }
 
-    // 指定文本文件路径
-    val inputFile = "simple/chenmengjuan.txt"  // 替换为你的实际文件路径
-
-    // 读取文本文件
-    val textFile = spark.read.textFile(inputFile)
-
-    // 拆分每行文本为单词
-    val words = textFile.flatMap(line => line.split("\\s+"))
-
-    // 统计每个单词出现的次数
-    val wordCounts = words.groupByKey(identity).count()
-
-    // 打印结果
-    wordCounts.show()
-
-    // 关闭 SparkSession
     spark.stop()
   }
 }
